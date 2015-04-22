ff931bc1c5f0a0aaf84d3b976ba85a8fd18d129c
vision <visionp1986@gmail.com>
2015-04-22 15:41:37 +0300

    add Class

---
+++ behaviors/UserIdCreatorBehavior.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace common\behaviors;
+
+use yii\db\BaseActiveRecord;
+use yii\db\Expression;
+use yii\behaviors\AttributeBehavior;
+
+
+class UserIdCreatorBehavior extends AttributeBehavior
+{
+
+    public $createdAtAttribute = 'user_id';
+    public $value;
+
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        if (empty($this->attributes)) {
+            $this->attributes = [
+                BaseActiveRecord::EVENT_BEFORE_INSERT => [$this->createdAtAttribute]
+            ];
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function getValue($event)
+    {
+        if ($this->value instanceof Expression) {
+            return $this->value;
+        } else {
+            return $this->value !== null ? call_user_func($this->value, $event) : \Yii::$app->user->getId();
+        }
+    }
+
+
+    public function touch($attribute)
+    {
+        $this->owner->updateAttributes(array_fill_keys((array) $attribute, $this->getValue(null)));
+    }
+}
